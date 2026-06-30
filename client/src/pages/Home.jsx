905d1fc6a4abb8e932ec21726208ab6bcf5f686e
shivpatel31n <shivpatel31n@gmail.com>
2026-06-30 18:17:42 +0530

    Navbar added

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Nexus Arena</h2>
+            <p>Manage and join esports tournaments.</p>
+        </div>
+    );
+}
+
+export default Home;
